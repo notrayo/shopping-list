1102afabc8eddd40d81430f57e5a6d997b57c1b4
notrayo <rayoleeroy@gmail.com>
2023-07-12 11:48:59 +0300

    created form for adding items

--- lib/screens/add_Item.dart
+++ lib/screens/add_Item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/models/cat_model.dart';
+import '../data/categories.dart';
 
 class AddItem extends StatefulWidget {
   const AddItem({super.key});
@@ -11,12 +13,57 @@ class _AddItemState extends State<AddItem> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Item to purchase : '),
-      ),
-      body: const Center(
-        child: Text('form to appear'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Add Item to purchase : '),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Enter name of item'),
+                ),
+                validator: (value) {
+                  return 'test ...';
+                },
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 15,
+                                  height: 15,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 8,
+                                ),
+                                Text(category.value.title)
+                              ],
+                            ))
+                    ], onChanged: (value) {}),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          label: Text('date and time ...')),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
